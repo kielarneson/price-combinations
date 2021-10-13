9f09209979f9ead1b32dfefb6c2e3368d160e161
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-13 16:33:17 -0500

    Copy pasted a working solution found online that uses recursion

--- decomposing.rb
+++ decomposing.rb
@@ -98,3 +98,24 @@ def three_items(array)
 end
 
 p three_items(menu_items_prices)
+
+# def subset_sum(numbers, target, partial = [])
+#   s = partial.inject 0, :+
+#   # check if the partial sum is equals to target
+
+#   puts "sum(#{partial})=#{target}" if s == target
+
+#   return if s >= target # if we reach the number why bother to continue
+
+#   (0..(numbers.length - 1)).each do |i|
+#     n = numbers[i]
+#     p "i = #{i}"
+#     p "n = #{n}"
+#     remaining = numbers.drop(i + 1)
+#     p "partial #{partial}"
+#     p "remaining = #{remaining}"
+#     subset_sum(remaining, target, partial + [n])
+#   end
+# end
+
+# subset_sum([3, 9, 8, 4, 5, 7, 10], 15)
